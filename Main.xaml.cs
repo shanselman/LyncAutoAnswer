12a7b0d27a2a19fb9898af00ef20d43788afb356
Scott Hanselman <scott@hanselman.com>
2012-07-03 16:04:39 -0700

    tidying and usings

--- Main.xaml.cs
+++ Main.xaml.cs
@@ -1,22 +1,5 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using Microsoft.Lync.Model;
-using Microsoft.Lync.Model.Conversation.AudioVideo;
-using System.Threading.Tasks;
-using Microsoft.Lync.Model.Conversation;
-using System.Runtime.InteropServices;
-using System.Diagnostics;
 
 namespace SuperSimpleLyncKiosk
 {
