9f865b2f79db15b9ecc0968befbb9a64d64a00d5
Scott Hanselman <scott@hanselman.com>
2012-08-10 02:30:49 -0700

    Only allow one instance

--- Not UI Suppressed/App.xaml.cs
+++ Not UI Suppressed/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Diagnostics;
 
 namespace SuperSimpleLyncKiosk
 {
@@ -11,5 +12,18 @@ namespace SuperSimpleLyncKiosk
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Get Reference to the current Process
+            Process me = Process.GetCurrentProcess();
+            // Check how many total processes have the same name as the current one
+            if (Process.GetProcessesByName(me.ProcessName).Length > 1)
+            {
+                Application.Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
     }
 }
